2416c9d295b1ceb8775c2f3aba72d8f734ce18d3
dfamrln <mhdffa1@gmail.com>
2025-06-10 15:16:23 +0700

    try

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 int main()
+{
+   try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+}
