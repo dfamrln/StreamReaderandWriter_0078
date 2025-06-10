72b361b73ac7d4f66ffef04b6e6f9d4f90e88321
dfamrln <mhdffa1@gmail.com>
2025-06-10 15:10:14 +0700

    end2

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <exception>
-// untuk objek exception yang akan digunakan
+//untuk  obyek exception yang akan digunankan
 #include <array>
-// untuk objek array yang akan kita gunakan
+//untuk obyek array yang akan kita gunakan
 using namespace std;
 
 int main()
 {
     cout << "Awal Program" << endl; // penanda 1:...
-    
-    try
-    {
-        //array<int, 3> data = {10, 20, 25};
-        int data[3] = {10, 20, 25};
+    try {
+        array<int, 3> data = { 10, 20, 30 };
         //pesan array integer 3 elemen
-        //cout << data.at(5) << endl;
-        cout << data[2] << endl;
+        cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
 
     }
-    catch(exception& e)
-    {
-        // penangkap menggunakan objek exception
-        cout << e.what() << endl;
-        /* akan dieksekusi karena array data hanya memiliki 3 elemen*/
-    }
-    cout << "Baris Program Yang Terakhir" << endl;
-    /* penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    cout << "Baris program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
     return 0;
 }
